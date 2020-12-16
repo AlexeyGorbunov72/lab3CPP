b9c8345307a065daeefa3b67a08296f46f9a6ad5
AnyDodic <korri.13@inbox.ru>
2020-12-16 04:34:44 +0300

    Initial Commit

---
+++ lab3CPP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lab3CPP
+//
+//  Created by Alexey on 16.12.2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
